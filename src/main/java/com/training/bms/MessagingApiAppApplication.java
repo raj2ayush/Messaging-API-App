c28032462ce80aeb584cd9ae8563eaf789bfed84
raj2ayush <ayush.raj2020@vitstudent.ac.in>
2024-07-12 15:08:56 +0530

    Fourth Change

--- src/main/java/com/training/bms/MessagingApiAppApplication.java
+++ src/main/java/com/training/bms/MessagingApiAppApplication.java
@@ -2,9 +2,15 @@ package com.training.bms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MessagingApiAppApplication {
+	
+	@GetMapping("enjoyWeekend")
+	public String sayHi() {
+		return "We are Going on the Vaccation this Weekend";
+	}
 	
 	public static String printGreetings() {
 		return "Hi Orcale";
